571bd43a19f617bdad8a1eb1d7784f9d94398291
kli-fi <61477292+kli-fi@users.noreply.github.com>
2021-03-26 17:20:13 +0100

    Create scenario_0.py

---
+++ scenario_0.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mars 25 09:32:22 2020
+@author: Fekher khelifi
+"""
+import serial
+import random
+import threading
+import time
+def result():
+  ser = serial.Serial('/dev/ttyUSB0', 38400, timeout=0)
+  while True:
+    localtime = time.localtime()
+    result = int (time.strftime("%S", localtime))
+    time.sleep(0.5)
+    A = ser.readline()
+#    print (A)
+    if len(A)>3:
+       print (result)
+       print (A)
+       f = open("zoo.txt", "a+")
+       f.writelines(str(A))
+       f.write("\r\n")
+def pay ():
+  ser = serial.Serial('/dev/ttyUSB0', 38400, timeout = 0)
+  n = 0
+  while n <255:
+    n += 1
+    payload = format (n, '02x')
+    command = ("*TRA,2,2,2,FF"+payload+",>")+'\r\n'
+    ser.write(command.encode())
+    time.sleep(10)
+
+t_1 = threading.Thread(name='result', target=result)
+t_2 = threading.Thread(name='pay', target=pay)
+t_1.start()
+t_2.start()
+
+t_1.join()
+t_2.join()
+
